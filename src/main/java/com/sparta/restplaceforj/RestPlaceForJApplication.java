ab712b87e97b0f5f96f3339fa8c20ecbcb1175c2
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-07-19 16:00:37 +0900

    init commit

---
+++ src/main/java/com/sparta/restplaceforj/RestPlaceForJApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.restplaceforj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlaceForJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestPlaceForJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/restplaceforj/RestPlaceForJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.restplaceforj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPlaceForJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
