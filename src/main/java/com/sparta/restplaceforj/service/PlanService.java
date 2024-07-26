17d4e1798c48bc6f3bed1e41b29af4287518827b
Delphinium52 <63048093+Delphinium52@users.noreply.github.com>
2024-07-26 17:40:21 +0900

    feat : 트랜젝션 설정

--- src/main/java/com/sparta/restplaceforj/service/PlanService.java
+++ src/main/java/com/sparta/restplaceforj/service/PlanService.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class PlanService {
 
   private final PlanRepository planRepository;
