dc999a0f1a7707e2365cd33a8b8d77b042d62bcf
Delphinium52 <63048093+Delphinium52@users.noreply.github.com>
2024-07-28 21:25:12 +0900

    feat : 플랜 조회

--- src/main/java/com/sparta/restplaceforj/repository/CoworkerRepository.java
+++ src/main/java/com/sparta/restplaceforj/repository/CoworkerRepository.java
@@ -1,8 +1,12 @@
 package com.sparta.restplaceforj.repository;
 
+import com.sparta.restplaceforj.dto.PlanResponseDto;
 import com.sparta.restplaceforj.entity.Coworker;
+import com.sparta.restplaceforj.entity.Plan;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CoworkerRepository extends JpaRepository<Coworker, Long> {
 
+  List<Plan> findAllByUserId(Long userId);
 }
