a3363d7d73279b935c6d651d861a703d963cca95
EugeneChoi <100894921+geneeuchoi@users.noreply.github.com>
2024-07-30 07:22:39 +0900

    refactor: LoginHandler 관련 config 수정

--- src/main/java/com/sparta/restplaceforj/jwt/JwtLogoutHandler.java
+++ src/main/java/com/sparta/restplaceforj/jwt/JwtLogoutHandler.java
@@ -4,17 +4,20 @@ import com.sparta.restplaceforj.entity.User;
 import com.sparta.restplaceforj.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class JwtLogoutHandler implements LogoutHandler {
+public class JwtLogoutHandler implements LogoutHandler, LogoutSuccessHandler {
 
     private final JwtUtil jwtUtil;
     private final UserRepository userRepository;
@@ -24,8 +27,9 @@ public class JwtLogoutHandler implements LogoutHandler {
             HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         log.info("로그아웃 시도");
 
-        // accessToken, refreshToken 가져오기
-        String refreshTokenValue = jwtUtil.getRefreshTokenFromHeader(request);
+    // accessToken, refreshToken 가져오기
+    String accessTokenValue = jwtUtil.getAccessTokenFromHeader(request);
+    String refreshTokenValue = jwtUtil.getRefreshTokenFromHeader(request);
 
         // 유저를 찾고, 유저의 refreshToken을 null로 set
         String email = jwtUtil.getUserInfoFromToken(refreshTokenValue).getSubject();
@@ -33,8 +37,16 @@ public class JwtLogoutHandler implements LogoutHandler {
         findUser.setRefreshToken(null);
         userRepository.save(findUser);
 
-        // SecurityContextHolder 초기화
-        SecurityContextHolder.clearContext();
-    }
+    // SecurityContextHolder 초기화
+    SecurityContextHolder.clearContext();
+  }
+
+  @Override
+  public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) throws IOException {
+    log.info("로그아웃 성공");
+    response.setStatus(HttpStatus.OK.value());
+    response.getWriter().write("로그아웃 성공");
+  }
 }
 
