624c88d12494b39f18b1f173c2fa97a53ed6d48d
eugeneChoi <chyj2108@naver.com>
2024-07-30 16:20:13 +0900

    refactor: 애너테이션 제거

--- src/main/java/com/sparta/restplaceforj/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/restplaceforj/security/UserDetailsServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
