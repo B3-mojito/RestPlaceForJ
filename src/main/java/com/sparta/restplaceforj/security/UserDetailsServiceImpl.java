ae6118764344def4f8925058dcdbea0f64af88fd
eugeneChoi <chyj2108@naver.com>
2024-07-30 11:31:34 +0900

    test: 회원 탈퇴시 로그인 불가하도록 예외 처리 추가

--- src/main/java/com/sparta/restplaceforj/security/UserDetailsServiceImpl.java
+++ src/main/java/com/sparta/restplaceforj/security/UserDetailsServiceImpl.java
@@ -1,14 +1,19 @@
 package com.sparta.restplaceforj.security;
 
 import com.sparta.restplaceforj.entity.User;
+import com.sparta.restplaceforj.entity.UserStatus;
+import com.sparta.restplaceforj.exception.CommonException;
+import com.sparta.restplaceforj.exception.ErrorEnum;
 import com.sparta.restplaceforj.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
@@ -18,6 +23,10 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(email));
+
+        if(user.getUserStatus() == UserStatus.DEACTIVATE) {
+            throw new CommonException(ErrorEnum.FORBIDDEN_ACCESS);
+        }
 
         return new UserDetailsImpl(user);
     }
