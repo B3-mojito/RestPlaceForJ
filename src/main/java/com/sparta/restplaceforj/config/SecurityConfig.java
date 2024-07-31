d1f1f9fbc42b9d54ee9669f449226e96f7877830
dobam <ckehqja@gmail.com>
2024-07-31 23:46:42 +0900

    fix : 임시로 수정한거 복구

--- src/main/java/com/sparta/restplaceforj/config/SecurityConfig.java
+++ src/main/java/com/sparta/restplaceforj/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
         authorizeHttpRequests
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
             .permitAll() // resources 접근 허용 설정
-            .requestMatchers("/v1").permitAll()
+            .requestMatchers("/v1/users").permitAll()
             .requestMatchers("/v1/users/login").permitAll()
             .anyRequest().authenticated() // 그 외 모든 요청 인증처리
     );
