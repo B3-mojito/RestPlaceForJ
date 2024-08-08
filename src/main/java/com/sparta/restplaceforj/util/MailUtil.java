99ffd1e1cebe703d4aab3be4878a3b006a2eaad8
eugeneChoi <chyj2108@naver.com>
2024-08-08 18:50:46 +0900

    refactor: String에서 StringBuilder로 변경, test url 변경

--- src/main/java/com/sparta/restplaceforj/util/MailUtil.java
+++ src/main/java/com/sparta/restplaceforj/util/MailUtil.java
@@ -32,13 +32,13 @@ public class MailUtil {
         message.setFrom(configEmail); //보내는 사람 메일 주소
         message.setSubject("[J의 안식처] 인증번호"); // 메일 제목
 
-        String msg="";
-        msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">[J의 안식처] 여행 계획 공동 작업자 초대</h1>";
-        msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 링크로 접속해서 인증을 완료하세요!</p>";
-        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
-        msg += inviteLink;
-        msg += "</td></tr></tbody></table></div>";
-        message.setText(msg, "utf-8", "html");
+        StringBuilder msg = new StringBuilder();
+        msg.append("<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">[J의 안식처] 여행 계획 공동 작업자 초대</h1>");
+        msg.append("<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 링크로 접속해서 인증을 완료하세요!</p>");
+        msg.append("<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">");
+        msg.append(inviteLink);
+        msg.append("</td></tr></tbody></table></div>");
+        message.setText(msg.toString(), "utf-8", "html");
 
         return message;
     }
