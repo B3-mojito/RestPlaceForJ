2b94f51f5105a1a6ed83ac3fc2cad6d0a4874b46
Delphinium52 <63048093+Delphinium52@users.noreply.github.com>
2024-08-15 12:20:17 +0900

    fix 프론트 연결을 위한 dto 수정

--- src/main/java/com/sparta/restplaceforj/dto/UserProfileResponseDto.java
+++ src/main/java/com/sparta/restplaceforj/dto/UserProfileResponseDto.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 
 @Getter
 public class UserProfileResponseDto {
-    private final String nickname;
-    private final String bio;
-    private final String profileImage;
 
-    @Builder
-    public UserProfileResponseDto(String nickname, String bio, String profileImage) {
-        this.nickname = nickname;
-        this.bio = bio;
-        this.profileImage = profileImage;
-    }
+  private final Long userId;
+  private final String nickname;
+  private final String bio;
+  private final String profileImage;
+
+  @Builder
+  public UserProfileResponseDto(Long userId, String nickname, String bio, String profileImage) {
+    this.userId = userId;
+    this.nickname = nickname;
+    this.bio = bio;
+    this.profileImage = profileImage;
+  }
 }
